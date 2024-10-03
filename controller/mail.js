810d259fecd07c4b6a5ffbb5978c64e32090512a
Rohan Gupta <rohangupta30128@gmail.com>
2024-10-03 16:22:25 +0530

    hide subject with next step

--- controller/mail.js
+++ controller/mail.js
@@ -13,7 +13,7 @@ const sendMail = (email,name,subject)=>{
         from : "rohangupta30128@gmail.com",
         to  : email,
         subject : "Your Inquiry",
-        html : '<h3>Hi '+name+'</h3> <p>I hope this message finds you well.Thank you for reaching out! I appreciate your interest in contacting me. I wanted to let you know that I will contact you soon to discuss '+subject+'<br /><br /> <p>Best regards,</p> <p>Rohan Gupta <br /> +918839786863 </p></p>'
+        html : '<h3>Hi '+name+'</h3> <p>I hope this message finds you well.Thank you for reaching out! I appreciate your interest in contacting me. I wanted to let you know that I will contact you soon to discuss next steps. <br /><br /> <p>Best regards,</p> <p>Rohan Gupta <br /> +918839786863 </p></p>'
     }
 
     transporter.sendMail(mailOption,(err,res)=>{
